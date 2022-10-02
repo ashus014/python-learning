c2646fa16ad90f5b3a7a329d83d7e1332cfae87e
Ashu Singh <ashu.ask.singh@gmail.com>
2022-10-02 11:44:48 +0530

    Initial commit

---
+++ day-01.py
@@ -0,0 +1,8 @@
+###########   Day 1 Learning
+# print("Ashu\nSingh")12
+
+# print(int(6/3))
+
+
+# score = 1234
+# print(f"Your score is {score}")
---
+++ day-03.py
@@ -0,0 +1,10 @@
+
+###########   Day 3 Learning
+
+print(" :: Welcome :: ")
+height = int(input("What is your height in cm ?"))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("sorry you can't ride")
