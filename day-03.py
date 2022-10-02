24996a64c6ed9da28e04725297bccbd5e67ab6ea
Ashu Singh <ashu.ask.singh@gmail.com>
2022-10-02 11:55:55 +0530

    learn elif in python

--- day-03.py
+++ day-03.py
@@ -1,10 +1,28 @@
 
 ###########   Day 3 Learning
 
+# print(" :: Welcome :: ")
+# height = int(input("What is your height in cm ?"))
+
+# if height > 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("sorry you can't ride")
+
+print(7%3)
+
+#nested if-else statement
 print(" :: Welcome :: ")
 height = int(input("What is your height in cm ?"))
 
 if height > 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age ? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <=18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("sorry you can't ride")
