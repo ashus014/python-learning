0de680f7752bf8108ff66bd3141915c382cc9ed5
Ashu Singh <ashu.ask.singh@gmail.com>
2022-11-05 00:33:26 +0530

    day 6 : functions and while loop

---
+++ day-06.py
@@ -0,0 +1,17 @@
+# working with functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+#my_function()
+
+
+# while loops
+
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    print(number_of_hurdles)
+    number_of_hurdles -= 1
+
+
